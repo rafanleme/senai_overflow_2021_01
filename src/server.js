30930202fbbb600aa78563e7fadea19143f5aa33
rafanleme <rafanleme@gmail.com>
2021-02-22 16:43:02 -0300

    Alterando a porta Heroku

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 const app = require("./app");
 
 //porta do servidor HTTP
-const PORT = 3333;
+const PORT = process.env.PORT || 3333;
 
 //subindo o servidor na web
 app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
